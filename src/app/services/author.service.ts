393b317d763a829851358bc458ad8b84405eb4a2
sona <sona.arshakyan.00@mail.ru>
2022-08-06 16:11:28 +0400

    I worked on already refactored branch for Routing and added Auth services

---
+++ src/app/services/author.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import { Author } from '../models/author';
+
+@Injectable({    
+    providedIn: 'root'    
+  }) 
+export class AuthorsService {
+    private authors: Author[];
+
+  getAllAuthors(): Observable<Author[]> {
+   return new Observable(obs => obs.next(this.authors));
+  }
+
+  getAuthor(id: number ): Observable<Author> {
+    return  new Observable( obs =>
+        obs.next(this.authors.filter((obj) => {
+            return obj.id === id; })[0] ));   
+        }
+
+  constructor() {
+    this.authors = [
+        {
+            name: "Stephen King",
+             id : 1
+        },
+        {
+            name : "George Orwell",
+            id : 2
+        },
+        {
+            name : "Virginia Woolf",
+            id : 3
+        },
+        {
+            name : "Kurt Vonnegut",
+            id : 4
+        },
+        {
+            name : "John Steinbeck",
+            id : 5
+        }
+    ]
+  }
+}
